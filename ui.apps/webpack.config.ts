5a16bc01bb9db66f6b21eee72f89d4ca5b0d3d7d
Tom Peeters <tomhpeeters@gmail.com>
2020-02-24 16:29:18 +0100

    build: add vuejs support

--- ui.apps/webpack.config.ts
+++ ui.apps/webpack.config.ts
@@ -1,6 +1,7 @@
 // webpack.config.js
 
 import * as webpack from 'webpack';
+import { VueLoaderPlugin } from 'vue-loader';
 
 const CLIENTLIB_ROOT = './src/main/content/jcr_root/apps/aem-vue-demo/clientlibs';
 const CLIENTLIB_NAME = 'clientlib-site';
@@ -14,6 +15,31 @@ function createConfig(clientlibRoot: string, clientlib: string): webpack.Configu
             path: `${__dirname}/${clientlibRoot}/${clientlib}/dist`,
             filename: './[name].js'
         },
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader'
+                },
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/,
+                    options: {
+                        appendTsSuffixTo: [/\.vue$/],
+                    }
+                },
+            ]
+        },
+        resolve: {
+            alias: {
+                vue$: 'vue/dist/vue.esm.js'
+            },
+            extensions: ['.ts', '.tsx', '.vue', '.json']
+        },
+        plugins: [
+            new VueLoaderPlugin()
+        ]
     };
 }
 
