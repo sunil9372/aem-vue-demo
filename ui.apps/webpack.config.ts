7d2b56ca8dc8da91a0c94c4f11c8a86d27527b74
Tom Peeters <tomhpeeters@gmail.com>
2020-02-24 16:27:00 +0100

    build: convert webpack config to typescript

--- ui.apps/webpack.config.ts
+++ ui.apps/webpack.config.ts
@@ -1,9 +1,11 @@
 // webpack.config.js
 
+import * as webpack from 'webpack';
+
 const CLIENTLIB_ROOT = './src/main/content/jcr_root/apps/aem-vue-demo/clientlibs';
 const CLIENTLIB_NAME = 'clientlib-site';
 
-function createConfig(clientlibRoot, clientlib) {
+function createConfig(clientlibRoot: string, clientlib: string): webpack.Configuration {
     return {
         entry: {
             bundle: `./${clientlibRoot}/${clientlib}/webpack-entry.js`
