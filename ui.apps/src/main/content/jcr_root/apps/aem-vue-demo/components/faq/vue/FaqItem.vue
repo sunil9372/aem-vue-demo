255b84b280f02e66444d8457bfe42eec9f2a5c57
Tom Peeters <tomhpeeters@gmail.com>
2020-03-18 15:15:25 +0100

    feat: create FaqItem Vue component

---
+++ ui.apps/src/main/content/jcr_root/apps/aem-vue-demo/components/faq/vue/FaqItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="faq-item">
+        <h4 class="faq-question">
+            <a href="#" @click.stop.prevent="expand">
+                <slot name="question"></slot>
+                <span class="caret" :style="{display: isOpen?'block':'none'}"></span>
+            </a>
+        </h4>
+        <div class="faq-answer" :style="{display: isOpen?'block':'none'}">
+            <p><slot name="answer"></slot></p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import { Component } from 'vue-property-decorator';
+
+    @Component
+    export default class FaqItem extends Vue {
+
+        private isOpen = false;
+
+        expand() {
+            this.isOpen = !this.isOpen;
+            return false;
+        }
+
+    };
+</script>
+
--- ui.apps/src/main/content/jcr_root/apps/aem-vue-demo/components/faq/vue/index.ts
+++ ui.apps/src/main/content/jcr_root/apps/aem-vue-demo/components/faq/vue/index.ts
@@ -1,4 +1,6 @@
 import Vue from 'vue';
 import FaqList from './FaqList.vue';
+import FaqItem from './FaqItem.vue';
 
 Vue.component('avd-faq-list', FaqList);
+Vue.component('avd-faq-item', FaqItem);
