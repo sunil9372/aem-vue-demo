c90d3246cc69d9bfa4c2e6d28999419a14f4b641
Tom Peeters <tomhpeeters@gmail.com>
2020-03-18 14:06:34 +0100

    feat: create FaqList Vue component

---
+++ ui.apps/src/main/content/jcr_root/apps/aem-vue-demo/components/faq/vue/FaqList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import { Component, Prop } from 'vue-property-decorator';
+
+    @Component
+    export default class FaqList extends Vue {
+        @Prop() private title!: string;
+    };
+</script>
---
+++ ui.apps/src/main/content/jcr_root/apps/aem-vue-demo/components/faq/vue/index.ts
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import FaqList from './FaqList.vue';
+
+Vue.component('avd-faq-list', FaqList);
